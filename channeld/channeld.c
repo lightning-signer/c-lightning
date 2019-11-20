156054b6bffbc67fe5cc048d3857263582df0681
Ken Sedgwick <ken@bonsai.com>
2019-11-19 16:00:35 -0800

    Fixed deref bug exposed by sendpay unit test.

--- channeld/channeld.c
+++ channeld/channeld.c
@@ -995,7 +995,7 @@ static secp256k1_ecdsa_signature *calc_commitsigs(const tal_t *ctx,
 						   &peer->channel->funding_pubkey[REMOTE],
 						   *txs[0]->input_amounts[0],
 						   (const struct witscript **)
-                               *txs[0]->output_witscripts);
+                               txs[0]->output_witscripts);
 
 	msg = hsm_req(tmpctx, take(msg));
 	if (!fromwire_hsm_sign_tx_reply(msg, commit_sig))
