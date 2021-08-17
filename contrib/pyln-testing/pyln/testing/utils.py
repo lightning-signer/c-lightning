f9c4c1fa3384ec993a83ed44338a3c70207a6313
Ken Sedgwick <ken@bonsai.com>
2021-08-16 21:12:18 -0700

    Set remote_signer logging to DEBUG for integration tests.

--- contrib/pyln-testing/pyln/testing/utils.py
+++ contrib/pyln-testing/pyln/testing/utils.py
@@ -519,8 +519,8 @@ class RemoteSignerD(TailableProc):
         TailableProc.__init__(self, rsignerd_dir)
         self.executable = env("REMOTE_SIGNER_CMD", 'rsignerd')
         self.opts = [
-            '--log-level-console=TRACE',
-            '--log-level-disk=TRACE',
+            '--log-level-console=DEBUG',
+            '--log-level-disk=DEBUG',
             '--datadir={}'.format(rsignerd_dir),
             '--port={}'.format(rsignerd_port),
         ]
