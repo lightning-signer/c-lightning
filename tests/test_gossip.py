1374fd523ac6e892da6d4d47eafa55d00e92929b
Ken Sedgwick <ken@bonsai.com>
2022-05-09 08:22:32 -0700

    Skip test with IPv6 issue in CI runner

--- tests/test_gossip.py
+++ tests/test_gossip.py
@@ -182,6 +182,8 @@ def test_announce_dns_suppressed(node_factory, bitcoind):
     assert addresses[0]['port'] == 1236
 
 
+@pytest.mark.developer("gossip without DEVELOPER=1 is slow")
+@unittest.skipIf(os.getenv('SUBDAEMON').startswith('hsmd:remote_hsmd'), "trouble with IPv6 in VLS CI runner")
 def test_announce_and_connect_via_dns(node_factory, bitcoind):
     """ Test that DNS annoucements propagate and can be used when connecting.
 
