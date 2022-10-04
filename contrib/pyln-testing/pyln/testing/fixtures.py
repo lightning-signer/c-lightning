9c02119b0174d355fd05dbecd3d8fc682b3348e9
Devrandom <c1.devrandom@niftybox.net>
2022-10-04 10:24:05 +0200

    Integrate lssd

--- contrib/pyln-testing/pyln/testing/fixtures.py
+++ contrib/pyln-testing/pyln/testing/fixtures.py
@@ -1,6 +1,6 @@
 from concurrent import futures
 from pyln.testing.db import SqliteDbProvider, PostgresDbProvider
-from pyln.testing.utils import NodeFactory, BitcoinD, ElementsD, env, LightningNode, TEST_DEBUG
+from pyln.testing.utils import NodeFactory, BitcoinD, ElementsD, env, LightningNode, TEST_DEBUG, LssD
 from pyln.client import Millisatoshi
 from typing import Dict
 
@@ -165,6 +165,25 @@ def bitcoind(directory, teardown_checks):
     except Exception:
         bitcoind.proc.kill()
     bitcoind.proc.wait()
+
+
+@pytest.fixture
+def lssd(directory, teardown_checks):
+    lssd = LssD(directory)
+
+    try:
+        lssd.start()
+    except Exception:
+        lssd.stop()
+        raise
+
+    yield lssd
+
+    try:
+        lssd.stop()
+    except Exception:
+        lssd.proc.kill()
+    lssd.proc.wait()
 
 
 class TeardownErrors(object):
@@ -449,11 +468,12 @@ def jsonschemas():
 
 
 @pytest.fixture
-def node_factory(request, directory, test_name, bitcoind, executor, db_provider, teardown_checks, node_cls, jsonschemas):
+def node_factory(request, directory, test_name, bitcoind, lssd, executor, db_provider, teardown_checks, node_cls, jsonschemas):
     nf = NodeFactory(
         request,
         test_name,
         bitcoind,
+        lssd,
         executor,
         directory=directory,
         db_provider=db_provider,
