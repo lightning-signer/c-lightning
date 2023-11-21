1ef10a8a9de4b5a47ca9597a64128074aff45f40
Ken Sedgwick <ken@bonsai.com>
2023-11-21 10:09:44 -0800

    tests: skip tests incompatible with VLS

--- tests/test_renepay.py
+++ tests/test_renepay.py
@@ -247,6 +247,7 @@ def test_amounts(node_factory):
     assert invoice["amount_received_msat"] >= Millisatoshi(123456)
 
 
+@unittest.skipIf(os.getenv('SUBDAEMON').startswith('hsmd:remote_hsmd') and os.getenv('VLS_PERMISSIVE') != '1', "invoice with any amount")
 def test_limits(node_factory):
     """
     Topology:
@@ -355,6 +356,7 @@ def start_channels(connections):
             )
 
 
+@unittest.skipIf(os.getenv('SUBDAEMON').startswith('hsmd:remote_hsmd') and os.getenv('VLS_PERMISSIVE') != '1', "invoice with any amount")
 def test_hardmpp(node_factory):
     """
     Topology:
